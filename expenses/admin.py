738fa6d2d11aa41e0136e9a53f8caa01d761007c
Rohit Kumar <kumar1rohit@outlook.com>
2024-10-20 15:14:40 +0530

    add expense admin

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Expense
 
-# Register your models here.
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "amount",
+        "paid_by",
+        "created_at",
+    )
+    search_fields = ("title", "paid_by")
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at", "updated_at")
+
+
+admin.site.register(Expense, ExpenseAdmin)
